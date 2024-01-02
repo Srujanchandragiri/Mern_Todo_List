a53a8b4c64b42b94dccf4c27eba63184bc129c06
Srujanchandragiri <145650122+Srujanchandragiri@users.noreply.github.com>
2024-01-02 17:39:57 +0530

    Add files via upload

---
+++ Server/index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const TodoModel = require('./Models/Todo')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+//database connection
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+.then(() =>  console.log('Db connected'))
+
+.catch(err => console.log(err))
+
+
+
+
+app.get('/get', (req,res)=>{
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+
+})
+
+app.put('/update/:id', (req,res) =>{
+    const{id} = req.params; // using this params we will grab our id
+   // console.log(id)
+   TodoModel.findByIdAndUpdate({_id: id}, {done: true})
+   .then(result => res.json(result))
+   .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req,res) =>{
+    const{id} = req.params;
+    TodoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+
+
+
+
+app.post('/add', (req, res)=>{
+
+  const task = req.body.task;
+  TodoModel.create({
+     task:task
+  }).then(result => res.json(result))
+   .catch(err => res.json(err))
+})
+app.listen(3001, () => {
+    console.log("Server is running")
+})
